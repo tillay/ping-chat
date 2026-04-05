4e0cdde47b663b8cd5435355a4c3f17a339b8746
tillay <tilleyy8@gmail.com>
2026-04-04 19:43:17 -0600

    decrease self message acceptance latency

--- client.go
+++ client.go
@@ -13,34 +13,42 @@ type ChatMessage struct {
 
 var lastTimestamp int
 
+func handleResponse(responseBytes []byte) {
+	passwordHashBytes := passHash(*pass)
+	responseStr := decryptFromBytes(responseBytes, passwordHashBytes)
+	var response MsgRecord
+	if err := json.Unmarshal(responseStr, &response); err != nil {
+		return
+	}
+	var incomingMsgJson ChatMessage
+	msgTextStr := decryptUsingPass(response.LastMsgEncrypted, *pass)
+	json.Unmarshal([]byte(msgTextStr), &incomingMsgJson)
+	if response.LastMsgTimestamp != lastTimestamp {
+		if incomingMsgJson.Message == "" && incomingMsgJson.User == "" {
+			tuiPrint("Chat begins here")
+		} else {
+			tuiPrint("[" + incomingMsgJson.Color + "]" + incomingMsgJson.User + "[white]: " + incomingMsgJson.Message)
+		}
+		lastTimestamp = response.LastMsgTimestamp
+	}
+}
+
 func runClientSender(msg string) {
 	msgJson := ChatMessage{Message: msg, User: *user, Color: *color}
 	jsonBytes, _ := json.Marshal(msgJson)
 	hash := passHash(*pass)
-	sendBytes(append(hash, encryptToBytes(jsonBytes, []byte(*pass))...), *ip)
+	responseBytes := sendBytes(append(hash, encryptToBytes(jsonBytes, []byte(*pass))...), *ip)
+	if responseBytes != nil {
+		handleResponse(responseBytes)
+	}
 }
 
 func runClientListener() {
 	for {
 		passwordHashBytes := passHash(*pass)
 		responseBytes := sendBytes(passwordHashBytes, *ip)
-		responseStr := decryptFromBytes(responseBytes, passwordHashBytes)
-		var response MsgRecord
-		err := json.Unmarshal(responseStr, &response)
-		if err != nil {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		var incomingMsgJson ChatMessage
-		msgTextStr := decryptUsingPass(response.LastMsgEncrypted, *pass)
-		json.Unmarshal([]byte(msgTextStr), &incomingMsgJson)
-		if response.LastMsgTimestamp != lastTimestamp {
-			if incomingMsgJson.Message == "" && incomingMsgJson.User == "" {
-				tuiPrint("Chat begins here")
-			} else {
-				tuiPrint("[" + incomingMsgJson.Color + "]" + incomingMsgJson.User + "[white]: " + incomingMsgJson.Message)
-			}
-			lastTimestamp = response.LastMsgTimestamp
+		if responseBytes != nil {
+			handleResponse(responseBytes)
 		}
 		time.Sleep(1 * time.Second)
 	}
