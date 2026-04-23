b1f4af409b70dae8a04823791403b18e2d3fd68f
tillay <tilleyy8@gmail.com>
2026-04-23 09:45:02 -0600

    add scrolling

--- tui.go
+++ tui.go
@@ -24,6 +24,7 @@ func newView() *tview.TextView {
 
 func initTUI(onSend func(string)) {
 	app = tview.NewApplication()
+	app.EnableMouse(true)
 	tview.Styles.PrimitiveBackgroundColor = tcell.ColorDefault
 
 	msgView, usersView, statusView = newView(), newView(), newView()
